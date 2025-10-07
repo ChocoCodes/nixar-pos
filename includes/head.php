5d564cfa6539b2301ec759d47d70f2987e88aac4
ChocoCodes <chocojhong999@gmail.com>
2025-10-07 08:58:39 +0800

    setup: initil commit

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $PageTitle ?></title>
+    <!-- Bootstrap & Font Awesome -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a0f7dcf96e.js" crossorigin="anonymous"></script>
+    <!-- Append modified timestamp after js/css file path - 
+    fix browser caching issues that prevents the js/css file to update --> 
+    <link rel="stylesheet" href='assets/css/styles.css?v=<?=filemtime('assets/css/styles.css')?>'>
+    <script defer src="assets/js/script.js?v=<?=filemtime('assets/js/script.js')?>"></script>
+</head>
+<body>
