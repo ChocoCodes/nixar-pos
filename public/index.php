67dde232541bc442b82a6311be7f8a56d67480ab
ChocoCodes <chocojhong999@gmail.com>
2025-10-08 20:53:16 +0800

    setup: add folders with placeholders

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php include_once 'includes/head.php'; ?>
-
-
+<?php echo password_hash('admin123', PASSWORD_DEFAULT); ?>
+<?php echo password_hash('cashier123', PASSWORD_DEFAULT); ?>
 <?php include_once 'includes/footer.php'; ?>
