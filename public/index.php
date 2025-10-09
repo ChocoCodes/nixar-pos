d27fc97950dab7a576e1fb8470303f32c7c14819
ChocoCodes <chocojhong999@gmail.com>
2025-10-09 08:04:52 +0800

    fix: inital setup error

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php include_once 'includes/head.php'; ?>
-<?php echo password_hash('admin123', PASSWORD_DEFAULT); ?>
-<?php echo password_hash('cashier123', PASSWORD_DEFAULT); ?>
-<?php include_once 'includes/footer.php'; ?>
+<?php include_once '../includes/head.php'; ?>
+<?php echo "admin123 " . password_hash('admin123', PASSWORD_DEFAULT) . "<br>"; ?>
+<?php echo "cashier123 " . password_hash('cashier123', PASSWORD_DEFAULT); ?>
+<?php include_once '../includes/footer.php'; ?>
