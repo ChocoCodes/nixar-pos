868937de8c4875cc48ced6c0277447bf9798d709
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-10-14 20:44:42 +0800

    chore: initial nav bar

--- public/cashier/pos.php
+++ public/cashier/pos.php
@@ -1,1 +1,9 @@
-<?php echo "cashier pos"; ?>
+<?php
+    $PageTitle = "Transaction | NIXAR POS";
+    $CssPath = "../assets/css/styles.css";
+?>
+<?php include_once '../../includes/head.php'; ?>
+    <div class="container-fluid py-3" style="height:100%;">
+        <?php include_once '../../includes/nav.php'; ?>
+    </div>
+<?php include_once '../../includes/footer.php'; ?>
