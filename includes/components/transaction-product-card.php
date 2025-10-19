135730eb2b724c1ac3c4d2c0d15b18c4de2ae290
ChocoCodes <chocojhong999@gmail.com>
2025-10-19 21:46:16 +0800

    chore: change product card border, schema product_name

--- includes/components/transaction-product-card.php
+++ includes/components/transaction-product-card.php
@@ -5,7 +5,7 @@
                 <img
                         src="<?= $dummyProduct['pic']; ?>"
                         alt="product imagea"
-                        class="img-fluid w-100 rounded bg-secondary object-fit-cover"
+                        class="img-fluid w-100 rounded-2 bg-secondary object-fit-cover"
                 />
             </div>
             <div class="w-50 px-3 py-2">
@@ -16,14 +16,14 @@
         <div class="w-100 d-flex align-items-center justify-content-between py-2">
             <h3>₱ <?= $dummyProduct['price']; ?></h3>
             <div class="position-relative d-flex align-items-center">
-                <button class="transaction-btn remove-btn position-absolute start-0">
-                    <i class="fa-solid fa-minus"></i>
+                <button class="transaction-btn add-btn position-absolute start-0">
+                    <i class="fa-solid fa-plus"></i>
                 </button>
                 <div class="quantity-display px-4 py-1 rounded-pill">
                     s
                 </div>
-                <button class="transaction-btn add-btn position-absolute end-0">
-                    <i class="fa-solid fa-plus"></i>
+                <button class="transaction-btn remove-btn position-absolute end-0">
+                    <i class="fa-solid fa-minus"></i>
                 </button>
             </div>
         </div>
