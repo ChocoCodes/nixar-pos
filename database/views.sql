17153049099b5f945f8e77fd7b4d090f37c3ebe3
407proxy <jdlabistre@gmail.com>
2025-10-22 09:48:15 +0800

    chore: fixed view names

--- database/views.sql
+++ database/views.sql
@@ -21,7 +21,7 @@ JOIN inventory i ON np.nixar_product_sku = i.nixar_product_sku
 JOIN product_suppliers ps ON np.nixar_product_sku = ps.nixar_product_sku
 WHERE np.is_deleted = 0;
 
-CREATE OR REPLACE VIEW low_stock_items_list_metrics AS  
+CREATE OR REPLACE VIEW low_stock_items_view AS  
 SELECT  
     np.nixar_product_sku, 
     np.product_name, 
@@ -33,7 +33,7 @@ JOIN product_materials pm ON np.product_material_id = pm.product_material_id
 WHERE i.current_stock <= i.min_threshold 
 ORDER BY i.current_stock ASC;
 
-CREATE OR REPLACE VIEW most_sold_item_by_qty_list_metrics AS 
+CREATE OR REPLACE VIEW most_sold_item_by_qty_view AS 
 SELECT  
     np.product_name, 
     SUM(rd.quantity) AS total_quantity_sold 
@@ -44,7 +44,7 @@ GROUP BY np.nixar_product_sku, np.product_name, pm.category
 ORDER BY total_quantity_sold DESC 
 LIMIT 5; 
 
-CREATE OR REPLACE VIEW best_selling_item_by_revenue_list_metrics AS 
+CREATE OR REPLACE VIEW best_selling_item_by_revenue_view AS 
 SELECT  
     np.product_name, 
     ROUND(SUM(rd.quantity * np.mark_up), 2) AS total_revenue  
