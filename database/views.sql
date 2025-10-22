b8efe42904ab8f40506ca227eba0f754a2f9c1f3
Ignatius Javelona <tobyyjavelona@gmail.com>
2025-10-22 12:48:58 +0800

    fix: pushed deleted views

--- database/views.sql
+++ database/views.sql
@@ -26,6 +26,8 @@ JOIN inventory i ON np.nixar_product_sku = i.nixar_product_sku
 JOIN product_suppliers ps ON np.nixar_product_sku = ps.nixar_product_sku
 WHERE np.is_deleted = 0;
 
+-- ============== INVENTORY REPORT LIST METRICS ==============
+
 CREATE OR REPLACE VIEW low_stock_items_view AS  
 SELECT  
     np.nixar_product_sku, 
@@ -59,3 +61,32 @@ JOIN product_materials pm ON np.product_material_id = pm.product_material_id
 GROUP BY np.nixar_product_sku, np.product_name 
 ORDER BY total_revenue DESC 
 LIMIT 5; 
+
+-- ============== SALES REPORT METRICS ==================
+CREATE OR REPLACE VIEW sales_report_view AS 
+SELECT 
+	SUM(total_amount) AS total_revenue, 
+	COUNT(receipt_id) AS total_transactions,
+	AVG(total_amount) AS avg_transaction_value,
+    0 AS profit_performance
+FROM receipts AS r;
+
+-- ============== SALES REPORT LIST METRICS ==============
+
+CREATE OR REPLACE VIEW category_performance_view AS 
+SELECT 
+    category, 
+    COUNT(category) AS category_performance
+FROM product_materials
+GROUP BY category
+ORDER BY category_performance DESC
+LIMIT 5;
+
+CREATE OR REPLACE VIEW sales_by_time_view AS 
+SELECT
+	DATE_FORMAT(created_at, '%l %p') AS hour_label,  
+	COUNT(*) AS total_orders
+FROM receipts
+GROUP BY hour_label, HOUR(created_at)
+ORDER BY HOUR(created_at)
+LIMIT 5;
